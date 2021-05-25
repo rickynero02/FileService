4787b169a320bd6c973a280617527eb500a47f96
rickynero02 <riccardonisdi2002@libero.it>
2021-05-25 17:06:58 +0200

    Implementato e testato upload e download

--- src/main/java/com/fileservice/files/FileController.java
+++ src/main/java/com/fileservice/files/FileController.java
@@ -4,6 +4,7 @@ import com.fileservice.config.S3ClientConfigProperties;
 import com.fileservice.exceptions.DownloadFailedException;
 import com.fileservice.exceptions.UploadFailedException;
 import lombok.AllArgsConstructor;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,10 +20,7 @@ import software.amazon.awssdk.services.s3.S3AsyncClient;
 import software.amazon.awssdk.services.s3.model.*;
 
 import java.nio.ByteBuffer;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
@@ -41,44 +39,6 @@ public class FileController {
     }
 
     @PostMapping(path = "/upload")
-    public Mono<ResponseEntity<UploadResult>> uploadFile(@RequestHeader HttpHeaders headers,
-         @RequestBody Flux<ByteBuffer> body) {
-
-        long length = headers.getContentLength();
-        if (length < 0) {
-            throw new UploadFailedException(HttpStatus.BAD_REQUEST.value(),
-                    Optional.of("required header missing: Content-Length"));
-        }
-
-        MediaType mediaType = headers.getContentType();
-        if(mediaType == null) {
-            mediaType = MediaType.APPLICATION_OCTET_STREAM;
-        }
-
-        String fileKey = UUID.randomUUID().toString();
-        Map<String, String> metadata = new HashMap<>();
-
-        CompletableFuture<PutObjectResponse> future = asyncClient
-                .putObject(PutObjectRequest.builder()
-                    .bucket(properties.getBucket())
-                    .contentLength(length)
-                    .key(fileKey)
-                    .contentType(mediaType.toString())
-                    .metadata(metadata)
-                    .build(),
-                    AsyncRequestBody.fromPublisher(body));
-
-
-        return Mono.fromFuture(future)
-                .map(response -> {
-                    checkUploadResult(response);
-                    return ResponseEntity
-                        .status(HttpStatus.CREATED)
-                        .body(new UploadResult(HttpStatus.CREATED, new String[]{fileKey}));
-                });
-    }
-
-    @PostMapping(path = "/uploadMany")
     public Mono<ResponseEntity<UploadResult>> uploadMultiPart(@RequestHeader HttpHeaders headers,
         @RequestPart("file") Flux<FilePart> file) {
 
@@ -93,9 +53,6 @@ public class FileController {
 
         Map<String, String> metadata = new HashMap<>();
         String fileName = part.filename();
-        if(fileName == null) {
-            fileName = fileKey;
-        }
 
         metadata.put("filename", fileName);
         MediaType mediaType = headers.getContentType();
@@ -106,11 +63,92 @@ public class FileController {
         CompletableFuture<CreateMultipartUploadResponse> future = asyncClient
                 .createMultipartUpload(CreateMultipartUploadRequest.builder()
                 .bucket(bucketName)
+                .contentType(mediaType.toString())
                 .key(fileKey)
                 .metadata(metadata)
                 .build());
 
+        final UploadState uploadState = new UploadState(bucketName, fileKey);
 
+        return Mono
+                .fromFuture(future)
+                .flatMapMany(response -> {
+                    checkUploadResult(response);
+                    uploadState.setUploadId(response.uploadId());
+                    return part.content();
+                })
+                .bufferUntil(buffer -> {
+                    uploadState.setBuffered(uploadState.getBuffered()
+                            + buffer.readableByteCount());
+                    if(uploadState.getBuffered() >= properties.getMultiPartMinSize()) {
+                        uploadState.setBuffered(0);
+                        return true;
+                    }
+                    return false;
+                })
+                .map(this::concatBuffers)
+                .flatMap(buffer -> uploadPart(uploadState, buffer))
+                .onBackpressureBuffer()
+                .reduce(uploadState, (state, completedPart) -> {
+                    state.getCompleteParts().put(completedPart.partNumber(), completedPart);
+                    return state;
+                })
+                .flatMap(this::completeUpload)
+                .map(response -> {
+                    checkUploadResult(response);
+                    return uploadState.getFileKey();
+                });
+    }
+
+    private Mono<CompleteMultipartUploadResponse> completeUpload(UploadState state) {
+        CompletedMultipartUpload multipartUpload = CompletedMultipartUpload.builder()
+                .parts(state.getCompleteParts().values())
+                .build();
+
+        return Mono.fromFuture(asyncClient.completeMultipartUpload(
+                CompleteMultipartUploadRequest.builder()
+                    .bucket(state.getBucket())
+                    .uploadId(state.getUploadId())
+                    .multipartUpload(multipartUpload)
+                    .key(state.getFileKey())
+                    .build()));
+    }
+
+    private ByteBuffer concatBuffers(List<DataBuffer> buffers) {
+        int partSize = 0;
+        for (DataBuffer b : buffers) {
+            partSize += b.readableByteCount();
+        }
+
+        ByteBuffer partData = ByteBuffer.allocate(partSize);
+        buffers.forEach(buffer -> partData.put(buffer.asByteBuffer()));
+
+        partData.rewind();
+        return partData;
+    }
+
+    private Mono<CompletedPart> uploadPart(UploadState uploadState, ByteBuffer byteBuffer) {
+        uploadState.setPartCounter(uploadState.getPartCounter()+1);
+        final int partNumber = uploadState.getPartCounter();
+
+        CompletableFuture<UploadPartResponse> request = asyncClient.uploadPart(
+                UploadPartRequest.builder()
+                .bucket(uploadState.getBucket())
+                .key(uploadState.getFileKey())
+                .partNumber(partNumber)
+                .uploadId(uploadState.getUploadId())
+                .contentLength((long) byteBuffer.capacity())
+                .build(), AsyncRequestBody.fromPublisher(Mono.just(byteBuffer))
+        );
+
+        return Mono.fromFuture(request)
+            .map(uploadPartResult -> {
+                checkUploadResult(uploadPartResult);
+                return CompletedPart.builder()
+                        .eTag(uploadPartResult.eTag())
+                        .partNumber(partNumber)
+                        .build();
+            });
     }
 
     @GetMapping(path="/download/{filekey}")
@@ -154,19 +192,5 @@ public class FileController {
             return;
         }
         throw new DownloadFailedException(response);
-    }
-
-    public static class UploadResult {
-        private final String bucket;
-        private final String fileKey;
-
-        private String uploadId;
-        private int partCounter;
-        private Map<Integer, CompletedPart> completeParts = new HashMap<>();
-
-        public UploadResult(String bucket, String fileKey) {
-            this.bucket = bucket;
-            this.fileKey = fileKey;
-        }
     }
 }
