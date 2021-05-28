4cd194713760934e824bb941b249b4959617411f
rickynero02 <66067202+rickynero02@users.noreply.github.com>
2021-05-28 21:36:31 +0200

    Aggiunto @CrossOrigin

--- src/main/java/com/fileservice/categories/CategoryController.java
+++ src/main/java/com/fileservice/categories/CategoryController.java
@@ -10,7 +10,7 @@ import reactor.core.publisher.Flux;
 @CrossOrigin
 @AllArgsConstructor
 @RestController
-@RequestMapping("/api/v1/categories")
+@RequestMapping("api/v1/categories")
 public class CategoryController {
 
     private final CategoryService service;
