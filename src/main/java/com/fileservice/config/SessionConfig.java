9a9679da5e6fef68aa6de16b45a1c1ea756ce7d4
rickynero02 <66067202+rickynero02@users.noreply.github.com>
2021-05-29 16:48:00 +0200

    Configurazione Sessione

---
+++ src/main/java/com/fileservice/config/SessionConfig.java
@@ -0,0 +1,16 @@
+package com.fileservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.server.EnableRedisWebSession;
+
+@EnableRedisWebSession
+@Configuration
+public class SessionConfig {
+
+    @Bean
+    public LettuceConnectionFactory redisConnection() {
+        return new LettuceConnectionFactory();
+    }
+}
